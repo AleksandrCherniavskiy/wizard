7aa957f0b8aad8a4e1d2eae63ba8ade9218c002f
AleksandrCherniavskiy <aleksandr.cherniavskyi28@gmail.com>
2020-01-27 23:16:18 +0200

    added const StepsArray

---
+++ src/app/core/models/steps-array.ts
@@ -0,0 +1,23 @@
+export const StepsArray  = [
+  {
+    id: 1,
+    label: 'Genre',
+    isVisible: true
+  }, {
+    id: 2,
+    label: 'Subgenre',
+    isVisible: true
+  }, {
+    id: 3,
+    label: '...',
+    isVisible: true
+  }, {
+    id: 4,
+    label: 'Add new subgenre',
+    isVisible: false
+  }, {
+    id: 5,
+    label: 'Information',
+    isVisible: false
+  }
+];
