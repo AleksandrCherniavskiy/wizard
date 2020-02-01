688fb93cb0be0d19d82ab0f9f9fca37b0bfa8dd9
AleksandrCherniavskiy <aleksandr.cherniavskyi28@gmail.com>
2020-02-01 21:34:26 +0200

    added @Input() descriptionIsRequiredControl

--- src/app/wizard/information/information.component.ts
+++ src/app/wizard/information/information.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
+
+import { StepsArray } from '../../core/models/steps-array';
 
 @Component({
   selector: 'app-information',
@@ -8,7 +10,12 @@ import { FormGroup } from '@angular/forms';
 })
 export class InformationComponent implements OnInit {
   @Input() informationControl: FormGroup;
+  @Input() descriptionIsRequiredControl: FormControl;
+
   authorArray = ['Author 1', 'Author 2', 'Author 3'];
+  publisherArray = ['Publisher 1', 'Publisher 2', 'Publisher 3'];
+  formatArray = ['Format 1', 'Format 2', 'Format 3'];
+  editionLanguageArray = ['Edition language 1', 'Edition language 2', 'Edition language 3'];
 
   constructor() { }
 
